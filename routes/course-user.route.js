238e86f119683b94971e5c28a784c5d816719766
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-04-23 12:59:01 +0700

    Update course-user.route.js

--- routes/course-user.route.js
+++ routes/course-user.route.js
@@ -4,6 +4,7 @@ import userCourseModel from "../models/user-course.model.js";
 import userModel from "../models/user.model.js";
 import topCourseModel from "../models/topCourse.model.js";
 import chapterModel from "../models/chapter.model.js";
+import lessonModel from "../models/lesson.model.js";
 
 import wishlistModel from "../models/wishlist.model.js";
 import express from "express";
@@ -80,20 +81,16 @@ router.get("/detail/:id", async function (req, res) {
     }
   }
 
-  // display chapter
+  // display lesson preview
   const chapter = await chapterModel.findbyIDCourse(courseID);
-  let chapterdata = [];
-  let lessondata = [];
-
+  let data = null;
   if (chapter != null) {
-    for (let chapters of chapter) {
-      let data = await chapterModel.findbyID(chapters.ID_CHAPTER);
-
-      chapterdata.push({
-        data,
-      });
-    }
+    let lessonsdata = await lessonModel.findbyIDChapter(chapter[0].ID_CHAPTER);
+    if (lessonsdata != null) data = lessonsdata[0];
   }
+  //TEST
+  data = { LESSONNAME: "TEST LESSON", ID_LESSON: 43 };
+
   //display rate and ratenum
   let rate = 0;
   let num = 0;
@@ -134,7 +131,7 @@ router.get("/detail/:id", async function (req, res) {
     rate,
     instructor,
     num,
-    chapterdata,
+    data,
     isBought,
     isLogin,
     isSave,
@@ -147,7 +144,6 @@ router.get("/detail/:id/enroll", async function (req, res) {
   let isLogin = req.session.auth;
   let isInstructor = false;
   let isSave = false;
-
 
   const courseID = req.params.id || 0;
 
@@ -210,18 +206,15 @@ router.get("/detail/:id/enroll", async function (req, res) {
     }
   }
 
-  // display chapter
+  // display lesson preview
   const chapter = await chapterModel.findbyIDCourse(courseID);
-  let chapterdata = [];
-
+  let data = null;
   if (chapter != null) {
-    for (let chapters of chapter) {
-      let data = await chapterModel.findbyID(chapters.ID_CHAPTER);
-      chapterdata.push({
-        data,
-      });
-    }
+    let lessonsdata = await lessonModel.findbyIDChapter(chapter[0].ID_CHAPTER);
+    if (lessonsdata != null) data = lessonsdata[0];
   }
+  //TEST
+  data = { LESSONNAME: "TEST LESSON", ID_LESSON: 43 };
 
   //display rate and ratenum
   let rate = 0;
@@ -278,7 +271,7 @@ router.get("/detail/:id/enroll", async function (req, res) {
     rate,
     instructor,
     num,
-    chapterdata,
+    data,
     isBought,
     isSave,
     isLogin,
@@ -429,24 +422,23 @@ router.get("/detail/:id/save", async function (req, res) {
 });
 
 function sortObject(obj) {
-	let sorted = {};
-	let str = [];
-	let key;
-	for (key in obj){
-		if (obj.hasOwnProperty(key)) {
-		str.push(encodeURIComponent(key));
-		}
-	}
-	str.sort();
-    for (key = 0; key < str.length; key++) {
-        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  let sorted = {};
+  let str = [];
+  let key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key));
     }
-    return sorted;
+  }
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
+  return sorted;
 }
 
 router.post("/detail/:id/create_vnpay", async function (req, res) {
   let isLogin = req.session.auth;
-
 
   const courseID = req.params.id || 0;
 
@@ -469,25 +461,24 @@ router.post("/detail/:id/create_vnpay", async function (req, res) {
     realPrice = price - (price * sale) / 100;
   }
 
-  if(isLogin){
+  if (isLogin) {
     let date = new Date();
     let createDate = moment(date).format("YYYYMMDDHHmmss");
-  
+
     let ipAddr =
       req.headers["x-forwarded-for"] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       req.connection.socket.remoteAddress;
-  
+
     let tmnCode = "BHMKE9J4";
     let secretKey = "LDFNIVOKMOBTIDFVMBJTXXVMHETHVDIV";
     let vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     let returnUrl = "http://localhost:3000/category/course/vnpay_return";
     let orderId = moment(date).format("DDHHmmss");
     let amount = realPrice;
-  
-    let locale = "vn";
 
+    let locale = "vn";
 
     let currCode = "VND";
     let vnp_Params = {};
@@ -503,28 +494,27 @@ router.post("/detail/:id/create_vnpay", async function (req, res) {
     vnp_Params["vnp_ReturnUrl"] = returnUrl;
     vnp_Params["vnp_IpAddr"] = ipAddr;
     vnp_Params["vnp_CreateDate"] = createDate;
-   
-  
+
     vnp_Params = sortObject(vnp_Params);
-  
+
     let signData = querystring.stringify(vnp_Params, { encode: false });
     let hmac = crypto.createHmac("sha512", secretKey);
     let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
     vnp_Params["vnp_SecureHash"] = signed;
     vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-  
-    return res.redirect(vnpUrl);  
+
+    return res.redirect(vnpUrl);
   }
   res.redirect("/category/course/detail/" + courseID);
 });
 
-router.get('/vnpay_return', function (req, res, next) {
+router.get("/vnpay_return", function (req, res, next) {
   let vnp_Params = req.query;
 
-  let secureHash = vnp_Params['vnp_SecureHash'];
+  let secureHash = vnp_Params["vnp_SecureHash"];
 
-  delete vnp_Params['vnp_SecureHash'];
-  delete vnp_Params['vnp_SecureHashType'];
+  delete vnp_Params["vnp_SecureHash"];
+  delete vnp_Params["vnp_SecureHashType"];
 
   vnp_Params = sortObject(vnp_Params);
   let tmnCode = "BHMKE9J4";
@@ -532,72 +522,73 @@ router.get('/vnpay_return', function (req, res, next) {
 
   let signData = querystring.stringify(vnp_Params, { encode: false });
   let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
 
-  if(secureHash === signed){
-      //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+  if (secureHash === signed) {
+    //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
 
-      res.redirect("/category/course/detail/" + orderCourse + "/enroll");
-  } else{
+    res.redirect("/category/course/detail/" + orderCourse + "/enroll");
+  } else {
     res.redirect("/category/course/detail" + orderCourse);
   }
 });
 
-router.get('/vnpay_ipn', function (req, res, next) {
+router.get("/vnpay_ipn", function (req, res, next) {
   let vnp_Params = req.query;
-  let secureHash = vnp_Params['vnp_SecureHash'];
-  
-  let orderId = vnp_Params['vnp_TxnRef'];
-  let rspCode = vnp_Params['vnp_ResponseCode'];
+  let secureHash = vnp_Params["vnp_SecureHash"];
 
-  delete vnp_Params['vnp_SecureHash'];
-  delete vnp_Params['vnp_SecureHashType'];
+  let orderId = vnp_Params["vnp_TxnRef"];
+  let rspCode = vnp_Params["vnp_ResponseCode"];
+
+  delete vnp_Params["vnp_SecureHash"];
+  delete vnp_Params["vnp_SecureHashType"];
 
   vnp_Params = sortObject(vnp_Params);
   let secretKey = "LDFNIVOKMOBTIDFVMBJTXXVMHETHVDIV";
   let signData = querystring.stringify(vnp_Params, { encode: false });
   let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
-  
-  let paymentStatus = '0'; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
+  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+
+  let paymentStatus = "0"; // Giả sử '0' là trạng thái khởi tạo giao dịch, chưa có IPN. Trạng thái này được lưu khi yêu cầu thanh toán chuyển hướng sang Cổng thanh toán VNPAY tại đầu khởi tạo đơn hàng.
   //let paymentStatus = '1'; // Giả sử '1' là trạng thái thành công bạn cập nhật sau IPN được gọi và trả kết quả về nó
   //let paymentStatus = '2'; // Giả sử '2' là trạng thái thất bại bạn cập nhật sau IPN được gọi và trả kết quả về nó
-  
+
   let checkOrderId = true; // Mã đơn hàng "giá trị của vnp_TxnRef" VNPAY phản hồi tồn tại trong CSDL của bạn
   let checkAmount = true; // Kiểm tra số tiền "giá trị của vnp_Amout/100" trùng khớp với số tiền của đơn hàng trong CSDL của bạn
-  if(secureHash === signed){ //kiểm tra checksum
-      if(checkOrderId){
-          if(checkAmount){
-              if(paymentStatus=="0"){ //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
-                  if(rspCode=="00"){
-                      //thanh cong
-                      //paymentStatus = '1'
-                      // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
-                      res.status(200).json({RspCode: '00', Message: 'Success'})
-                  }
-                  else {
-                      //that bai
-                      //paymentStatus = '2'
-                      // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
-                      res.status(200).json({RspCode: '00', Message: 'Success'})
-                  }
-              }
-              else{
-                  res.status(200).json({RspCode: '02', Message: 'This order has been updated to the payment status'})
-              }
+  if (secureHash === signed) {
+    //kiểm tra checksum
+    if (checkOrderId) {
+      if (checkAmount) {
+        if (paymentStatus == "0") {
+          //kiểm tra tình trạng giao dịch trước khi cập nhật tình trạng thanh toán
+          if (rspCode == "00") {
+            //thanh cong
+            //paymentStatus = '1'
+            // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
+            res.status(200).json({ RspCode: "00", Message: "Success" });
+          } else {
+            //that bai
+            //paymentStatus = '2'
+            // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
+            res.status(200).json({ RspCode: "00", Message: "Success" });
           }
-          else{
-              res.status(200).json({RspCode: '04', Message: 'Amount invalid'})
-          }
-      }       
-      else {
-          res.status(200).json({RspCode: '01', Message: 'Order not found'})
+        } else {
+          res
+            .status(200)
+            .json({
+              RspCode: "02",
+              Message: "This order has been updated to the payment status",
+            });
+        }
+      } else {
+        res.status(200).json({ RspCode: "04", Message: "Amount invalid" });
       }
-  }
-  else {
-      res.status(200).json({RspCode: '97', Message: 'Checksum failed'})
+    } else {
+      res.status(200).json({ RspCode: "01", Message: "Order not found" });
+    }
+  } else {
+    res.status(200).json({ RspCode: "97", Message: "Checksum failed" });
   }
 });
-
 
 export default router;
